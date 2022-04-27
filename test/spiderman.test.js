29298b824d4a212a613fe06a0b346b831af3a22a
JossPerezT <jossperez44@gmail.com>
2022-04-27 12:30:13 -0500

    3.TDD. Paso 1. Escribiruna prueba según los requerimientos, esta prueba falla porque aún no existe la clase

--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -1,6 +1,14 @@
-describe ("Test Suite Dummy Description", () =>{
-    test ('Case 1 Dummy', () =>{
-        const resultOfSomething = 1+2+10+5
-        expect(resultOfSomething).toBe(18)
+describe ("Unit Tests for Spiderman class", () =>{
+    test ('Case 1. Create an spiderman object', () =>{
+        // Aqui vamos a escribir el código que queremos usar tal cual 
+        // Queremos poder instanciar un objeto Spiderman co esta información 
+        const andrewGarfield = new Spiderman ("Spiderman Sony", 31, "Andrew Garfield", 2, "Sony")
+        
+        // Validamos que el código funcione de la forma esperada
+        expect(andrewGarfield.name).toBe("Spiderman Sony")
+        expect(andrewGarfield.age).toBe(31)
+        expect(andrewGarfield.actor).toBe("Andrew Garfiel")
+        expect(andrewGarfield.movies).toBe(2)
+        expect(andrewGarfield.studio).toBe("Sony")
     })
 })
