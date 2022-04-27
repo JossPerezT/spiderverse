9f5ed6c8cf68abd1ddd4983f591f6c89b88b987c
JossPerezT <jossperez44@gmail.com>
2022-04-27 12:53:22 -0500

    3.TDD. Paso 3.1 Pasando todas las pruebas sin falla alguna

--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -9,7 +9,7 @@ describe ("Unit Tests for Spiderman class", () =>{
         // Validamos que el código funcione de la forma esperada
         expect(andrewGarfield.name).toBe("Spiderman Sony")
         expect(andrewGarfield.age).toBe(31)
-        expect(andrewGarfield.actor).toBe("Andrew Garfiel")
+        expect(andrewGarfield.actor).toBe("Andrew Garfield")
         expect(andrewGarfield.movies).toBe(2)
         expect(andrewGarfield.studio).toBe("Sony")
     })
