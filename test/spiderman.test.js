5459ae4b92dc8de39de25badc63d2c57e6355e3e
JossPerezT <jossperez44@gmail.com>
2022-04-27 13:09:19 -0500

    TDD. 4. Agregando una prueba para el nuevo requerimeinto. Falla porque no hay un método definido

--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -13,4 +13,10 @@ describe ("Unit Tests for Spiderman class", () =>{
         expect(andrewGarfield.movies).toBe(2)
         expect(andrewGarfield.studio).toBe("Sony")
     })
+
+    test ('Case 2. Create a method getInfo()', ()=> {
+        const tomHolland = new Spiderman ("Spiderman Marvel", 25, "Tom Holland", 5, "Marvel")
+
+        expect (tomHolland.getInfo()).toBe("Hey, I'm Tom Holland form Marver studio")
+    })
 })
